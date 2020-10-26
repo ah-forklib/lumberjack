0dae85f5680a4e97e2fe6b64a7955c82821e2558
AoiHosizora <a970335605@hotmail.com>
2020-10-26 21:33:29 +0800

    feat(example): add example_test.go

--- rotate_test.go
+++ rotate_test.go
@@ -9,18 +9,6 @@ import (
 	"syscall"
 )
 
-// To use lumberjack with the standard library's log package, just pass it into
-// the SetOutput function when your application starts.
-func Example() {
-	log.SetOutput(&Logger{
-		Filename:   "/var/log/myapp/foo.log",
-		MaxSize:    500, // megabytes
-		MaxBackups: 3,
-		MaxAge:     28,   // days
-		Compress:   true, // disabled by default
-	})
-}
-
 // Example of how to rotate in response to SIGHUP.
 func ExampleLogger_Rotate() {
 	l := &Logger{}
